092e6784903daafc65a6d22019231a7e6b377a42
Stewart A <strangelytyped+github-23f9bb@gmail.com>
2017-09-01 23:11:07 +0100

    Path subscriber rewrite

--- path-subscriber.js
+++ path-subscriber.js
@@ -1,41 +1,103 @@
 "use strict";
 
 const EventEmitter = require("events").EventEmitter;
+const _ = require("lodash");
+const NONEXISTENT_MARKER = require("./data-model").NONEXISTENT_MARKER;
 
 
-//TODO: if the change occurs higher in the tree than the subscriber is subscribed, then the subscriber should also be notified
-//But the event needs to be processed to obtain the right nested old/new values
+let isSubpathOf = function(sourcePath, targetPath){
+	if(sourcePath.length >= targetPath.length){
+		return _.isEqual(sourcePath.slice(0, targetPath.length), targetPath);
+	}
+	return false;
+};
+
+let isParentPathOf = function(sourcePath, targetPath){
+	if(sourcePath.length < targetPath.length){
+		return _.isEqual(targetPath.slice(0, sourcePath.length), sourcePath);
+	}
+	return false;
+};
+
+let traverse = function(obj, pathFragment){
+	if(obj == null || obj === NONEXISTENT_MARKER){
+		return NONEXISTENT_MARKER;
+	}
+	obj = obj[pathFragment];
+	if(obj === undefined){
+		obj = NONEXISTENT_MARKER
+	}
+	return obj;
+}
+
+let rebaseEvent = function(evt, subscriberPath){
+	let [origPath, oldVal, newVal] = evt;
+	let relativePath = subscriberPath.slice(origPath.length);
+	relativePath.forEach(pathFragment => {
+		oldVal = traverse(oldVal, pathFragment);
+		newVal = traverse(newVal, pathFragment);
+	});
+	if((oldVal !== NONEXISTENT_MARKER || newVal !== NONEXISTENT_MARKER) && oldVal !== newVal){
+		return [subscriberPath, oldVal, newVal];
+	}
+	return null;
+};
+
+let parsePath = function(path){
+	//This implementation does not guarantee a valid path has been provided
+	return _.toPath(path);
+};
 
 class PathSubscriber extends EventEmitter {
 	constructor(model){
 		super();
+		let subscriberMap = new Map();
+		this.on("removeListener", (eventName) => {
+			if(this.listenerCount(eventName) === 0){
+				subscriberMap.delete(eventName);
+			}
+		});
+		this.on("newListener", (eventName) => {
+			if(this.listenerCount(eventName) === 0){
+				let processedEventName = parsePath(eventName);
+				if(processedEventName != null){
+					subscriberMap.set(eventName, processedEventName);
+				}
+			}
+		});
 		model.on("change", (...evtArgs) => {
-			let subscriberPaths = this.eventNames();
 			//evtArgs either contains array of events (if we're wrapping the buffer) or array+val+val (if we aren't)
 			if(evtArgs.length === 1){
 				let evtList = evtArgs[0];
 				//each entry in evtList is a single event (path+val+val)
-				for(let subscriber of subscriberPaths){
-					let filteredEvents = evtList.filter((evt) => {
-						let changePath = evt[0].join(".");
-						return changePath.length >= subscriber.length && changePath.substr(0, subscriber.length) === subscriber;
-					});
+				for(let [subscriberName, subscriberPath] of subscriberMap){
+					let filteredSubEvents = evtList.filter(evt => isSubpathOf(evt[0], subscriberPath));
+					let filteredSuperEvents = evtList.filter(evt => isParentPathOf(evt[0], subscriberPath))
+						.map(evt => rebaseEvent(evt, subscriberPath))
+						.filter(evt => evt != null);
+					let filteredEvents = filteredSubEvents.concat(filteredSuperEvents);
 					if(filteredEvents.length){
-						this.emit(subscriber, filteredEvents);
+						this.emit(subscriberName, filteredEvents);
 					}
 				}
 			}else{
-				let [path, oldVal, newVal] = evtArgs;
+				let path = evtArgs[0];
 				//This is a single event
-				let changePath = path.join(".");
-				for(let subscriber of subscriberPaths){
-					if(changePath.length >= subscriber.length && changePath.substr(0, subscriber.length) === subscriber){
-						this.emit(subscriber, ...evtArgs);
+				for(let [subscriberName, subscriberPath] of subscriberMap){
+					if(isSubpathOf(path, subscriberPath)){
+						this.emit(subscriberName, ...evtArgs);
+					}else if(isParentPathOf(path, subscriberPath)){
+						let modifiedEvent = rebaseEvent(evtArgs, subscriberPath);
+						if(modifiedEvent !== null){
+							this.emit(subscriberName, ...modifiedEvent);
+						}
 					}
 				}
 			}
 		});
 	}
 };
+
+PathSubscriber.NONEXISTENT_MARKER = NONEXISTENT_MARKER;
 
 module.exports = PathSubscriber;
--- spec/path-subscriber.spec.js
+++ spec/path-subscriber.spec.js
@@ -1,6 +1,7 @@
 describe("path-subscriber", function() {
 	let PathSubscriber = require("../path-subscriber.js");
 	let EventEmitter = require("events").EventEmitter;
+	let NONEXISTENT_MARKER = PathSubscriber.NONEXISTENT_MARKER;
 
 	let rawEmitter;
 	let wrappedEmitter;
@@ -18,6 +19,8 @@ describe("path-subscriber", function() {
 			"path.to.object.property",
 			"path",
 			"path.to.object.property.toofar",
+			"path.to.object.property2",
+			"path.to.object.prop",
 			"path.to.other",
 			"path.to.other.property"
 		];
@@ -36,6 +39,8 @@ describe("path-subscriber", function() {
 			"path.to.object.property": expectedDispatchedEvent,
 			"path": expectedDispatchedEvent,
 			"path.to.object.property.toofar": [],
+			"path.to.object.property2": [],
+			"path.to.object.prop": [],
 			"path.to.other": [],
 			"path.to.other.property": []
 		});
@@ -49,6 +54,8 @@ describe("path-subscriber", function() {
 			"path.to.object.property",
 			"path",
 			"path.to.object.property.toofar",
+			"path.to.object.property2",
+			"path.to.object.prop",
 			"path.to.other",
 			"path.to.other.property"
 		];
@@ -87,8 +94,177 @@ describe("path-subscriber", function() {
 			"path.to.object.property": expectedDispatchedEvent1,
 			"path": expectedDispatchedEvent2,
 			"path.to.object.property.toofar": [],
+			"path.to.object.property2": [],
+			"path.to.object.prop": [],
 			"path.to.other": [],
 			"path.to.other.property": []
+		});
+	});
+
+	it("should un-nest single-events from higher in the object heirarchy to dispatch to affected deeper subscriptions", function() {
+		let receivedEvents = {};
+		let subscribedPaths = [
+			"path.to.object",
+			"path.to",
+			"path.to.object.property",
+			"path",
+			"path.to.object.property.toofar",
+			"path.to.object.property2",
+			"path.to.object.prop",
+			"path.to.other",
+			"path.to.other.property"
+		];
+
+		subscribedPaths.forEach((path) => {
+			receivedEvents[path] = [];
+			wrappedEmitter.on(path, (...evts) => receivedEvents[path].push(evts));
+		});
+
+		rawEmitter.emit("change", ["path", "to"], { object: { property: 5 } }, { other: { property: 6 } });
+
+		let expectedDispatchedEvent1 = [ [ [ "path", "to", "object", "property" ], 5, NONEXISTENT_MARKER ] ];
+		let expectedDispatchedEvent2 = [ [ [ "path", "to", "object" ], { property: 5 }, NONEXISTENT_MARKER ] ];
+		let expectedDispatchedEvent3 = [ [ [ "path", "to" ], { object: { property: 5 } }, { other: { property: 6 } } ] ];
+		let expectedDispatchedEvent4 = [ [ [ "path", "to", "other" ], NONEXISTENT_MARKER, { property: 6 } ] ];
+		let expectedDispatchedEvent5 = [ [ [ "path", "to", "other", "property" ], NONEXISTENT_MARKER, 6 ] ];
+		expect(receivedEvents).toEqual({
+			"path.to.object": expectedDispatchedEvent2,
+			"path.to": expectedDispatchedEvent3,
+			"path.to.object.property": expectedDispatchedEvent1,
+			"path": expectedDispatchedEvent3,
+			"path.to.object.property.toofar": [],
+			"path.to.object.property2": [],
+			"path.to.object.prop": [],
+			"path.to.other": expectedDispatchedEvent4,
+			"path.to.other.property": expectedDispatchedEvent5
+		});
+	});
+
+	it("should un-nest multi-events from higher in the object heirarchy to dispatch to affected deeper subscriptions", function() {
+		let receivedEvents = {};
+		let subscribedPaths = [
+			"path.to.object",
+			"path.to",
+			"path.to.object.property",
+			"path",
+			"path.to.object.property.toofar",
+			"path.to.object.property2",
+			"path.to.object.prop",
+			"path.to.other",
+			"path.to.other.property"
+		];
+
+		subscribedPaths.forEach((path) => {
+			receivedEvents[path] = [];
+			wrappedEmitter.on(path, (evts) => receivedEvents[path].push(evts));
+		});
+
+		rawEmitter.emit("change", [
+			[
+				["path", "to"],
+				{
+					object: {
+						property: 8
+					}
+				},
+				{
+					object: {
+						property: 9
+					}
+				}
+			]
+		]);
+
+		let expectedDispatchedEvent1 = [[
+			[ [ "path", "to" ], { object: { property: 8 } }, { object: { property: 9 } } ],
+		]];
+
+		let expectedDispatchedEvent2 = [[
+			[ [ "path", "to", "object" ], { property: 8 }, { property: 9 } ],
+		]];
+
+		let expectedDispatchedEvent3 = [[
+			[ [ "path", "to", "object", "property" ], 8, 9 ],
+		]];
+
+		expect(receivedEvents).toEqual({
+			"path.to.object": expectedDispatchedEvent2,
+			"path.to": expectedDispatchedEvent1,
+			"path.to.object.property": expectedDispatchedEvent3,
+			"path": expectedDispatchedEvent1,
+			"path.to.object.property.toofar": [],
+			"path.to.object.property2": [],
+			"path.to.object.prop": [],
+			"path.to.other": [],
+			"path.to.other.property": []
+		});
+	});
+
+	it("should use the sentinel value to indicate parts of the tree where no value remains when un-nesting events", function() {
+		let receivedEvents = {};
+		let subscribedPaths = [
+			"path.to.object",
+			"path.to",
+			"path.to.object.property",
+			"path",
+			"path.to.object.property.toofar",
+			"path.to.object.property2",
+			"path.to.object.prop",
+			"path.to.other",
+			"path.to.other.property"
+		];
+
+		subscribedPaths.forEach((path) => {
+			receivedEvents[path] = [];
+			wrappedEmitter.on(path, (evts) => receivedEvents[path].push(evts));
+		});
+
+		rawEmitter.emit("change", [
+			[
+				["path", "to"],
+				{
+					object: {
+						property: 8
+					}
+				},
+				{
+					other: {
+						property: 9
+					}
+				}
+			]
+		]);
+
+		let expectedDispatchedEvent1 = [[
+			[ [ "path", "to" ], { object: { property: 8 } }, { other: { property: 9 } } ],
+		]];
+
+		let expectedDispatchedEvent2 = [[
+			[ [ "path", "to", "object" ], { property: 8 }, NONEXISTENT_MARKER ]
+		]];
+
+		let expectedDispatchedEvent3 = [[
+			[ [ "path", "to", "object", "property" ], 8, NONEXISTENT_MARKER ]
+		]];
+
+		let expectedDispatchedEvent4 = [[
+			[ [ "path", "to", "other", "property" ], NONEXISTENT_MARKER, 9 ]
+		]];
+
+		let expectedDispatchedEvent5 = [[
+			[ [ "path", "to", "other" ], NONEXISTENT_MARKER, { property: 9 } ]
+		]];
+
+		expect(receivedEvents).toEqual({
+			"path.to.object": expectedDispatchedEvent2,
+			"path.to": expectedDispatchedEvent1,
+			"path.to.object.property": expectedDispatchedEvent3,
+			"path": expectedDispatchedEvent1,
+			"path.to.object.property.toofar": [],
+			"path.to.object.property2": [],
+			"path.to.object.prop": [],
+			"path.to.other": expectedDispatchedEvent5,
+			"path.to.other.property": expectedDispatchedEvent4
 		});
 	});
 });
